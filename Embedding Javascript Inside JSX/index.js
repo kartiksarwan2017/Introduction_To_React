abe9c15674b55b2175c4309caa8c9a0f816df320
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-07-22 13:44:55 +0530

    Embedding Javascript Inside JSX

---
+++ Embedding Javascript Inside JSX/index.js
@@ -0,0 +1,18 @@
+const App = () => {
+    return (
+       <>
+          <h1>Hello World!</h1>
+       </>
+    )
+}
+
+
+// function App() {
+//     return (
+//       <>
+//         <h1>Let's Learn JSX!</h1>
+//       </>
+//     );
+// }
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
